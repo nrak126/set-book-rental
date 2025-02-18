7cec96a97b9d1e86d5b4cb97918e1670e28e5511
nrak126 <daichidaichi126@gmail.com>
2025-02-18 18:06:53 +0900

    :art: コードの整形とインデントを修正

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,28 +1,28 @@
 "use client";
-import {useEffect, useState} from 'react';
-import type {Book} from '@/src/types/book';
+import { useEffect, useState } from "react";
+import type { Book } from "@/src/types/book";
 
 export default function Page() {
-	const [books, setBooks] = useState<Book[]>([]);
+  const [books, setBooks] = useState<Book[]>([]);
 
-	useEffect(() => {
-		(async () => {
-			const response = await fetch('/api/books');
-			const data = await response.json();
-			setBooks(data);
-		})();
-	}, []);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/books");
+      const data = await response.json();
+      setBooks(data);
+    })();
+  }, []);
 
-	return (
-		<>
-			<h1>本のリスト</h1>
-			<ul>
-				{books.map((book, index) => (
-					<li key={index}>
-						{book.title} ({book.author})
-					</li>
-				))}
-			</ul>
-		</>
-	)
+  return (
+    <>
+      <h1>本のリスト</h1>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>
+            {book.title} ({book.author})
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
